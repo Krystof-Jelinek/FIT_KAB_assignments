d793b49786edfdca1c06e6b0579a8d4f0dc0a4a7
Krystof Jelinek <krys.jelinek@seznam.cz>
2024-04-01 13:56:51 +0200

    finished second homework

--- progtest-du03-sifrovani-souboru.cpp
+++ progtest-du03-sifrovani-souboru.cpp
@@ -30,21 +30,33 @@ struct crypto_config
 	size_t m_IV_len;
 };
 
+void print_key_hex(const std::unique_ptr<uint8_t[]>& key, size_t length) {
+    for (size_t i = 0; i < length; ++i) {
+        std::cout << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(key[i]);
+    }
+    std::cout << std::endl;
+}
+
 #endif /* _PROGTEST_ */
 
 //Kryštof Jelínek -- jelinkry
 
+void generate_random_bytes(int length, uint8_t * in){
+    while (RAND_bytes(in, length) != 1) {
+    }
+    return;
+}
+
 bool encrypt_data ( const std::string & in_filename, const std::string & out_filename, crypto_config & config )
 {
 	// Ensure that required parameters are set
-    if (!config.m_crypto_function || !config.m_key || !config.m_key_len) {
+    if (!config.m_crypto_function) {
         return false;
     }
 
     EVP_CIPHER_CTX *ctx;
     const EVP_CIPHER *cipher;
 
-    // Initialize OpenSSL library
     OpenSSL_add_all_algorithms();
 
     // Create and initialize the context
@@ -60,20 +72,37 @@ bool encrypt_data ( const std::string & in_filename, const std::string & out_fil
         return false;
     }
 
+    size_t iv_lenght = EVP_CIPHER_iv_length(cipher);
+    size_t key_lenght = EVP_CIPHER_key_length(cipher);
+
+    if((key_lenght > config.m_key_len)||(config.m_key == nullptr)){
+        std::unique_ptr<uint8_t[]> new_key;
+        new_key = std::make_unique<uint8_t[]>(key_lenght);
+        generate_random_bytes(key_lenght, new_key.get());
+        config.m_key_len = key_lenght;
+        config.m_key = std::move(new_key);
+    }
+
+    if(((iv_lenght > 0) && (iv_lenght > config.m_IV_len))||((iv_lenght > 0)&&(config.m_IV == nullptr))){
+        std::unique_ptr<uint8_t[]> new_iv;
+        new_iv = std::make_unique<uint8_t[]>(iv_lenght);
+        generate_random_bytes(iv_lenght, new_iv.get());
+        config.m_IV_len = iv_lenght;
+        config.m_IV = std::move(new_iv);
+    }
+
     // Initialize the encryption operation
     if (EVP_EncryptInit_ex(ctx, cipher, NULL, config.m_key.get(), config.m_IV.get()) != 1) {
         EVP_CIPHER_CTX_free(ctx);
         return false;
     }
 
-    // Open input file
     std::ifstream inFile(in_filename, std::ios::binary);
     if (!inFile.is_open()) {
         EVP_CIPHER_CTX_free(ctx);
         return false;
     }
 
-    // Open output file
     std::ofstream outFile(out_filename, std::ios::binary);
     if (!outFile.is_open()) {
         inFile.close();
@@ -84,19 +113,39 @@ bool encrypt_data ( const std::string & in_filename, const std::string & out_fil
     //read the headers
     char headerBuf[18];
     inFile.read(headerBuf, sizeof(headerBuf));
+    if(!inFile){
+        inFile.close();
+        outFile.close();
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
     outFile.write(headerBuf, sizeof(headerBuf));
+    if(!outFile){
+        inFile.close();
+        outFile.close();
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
 
     // Encryption buffer
-    const size_t BLOCK_SIZE = 16; // AES block size
+    const size_t BLOCK_SIZE = 16;
     uint8_t inBuf[BLOCK_SIZE];
     uint8_t outBuf[BLOCK_SIZE + BLOCK_SIZE]; //for possible padding
 
     int outLen;
     while (!inFile.eof()) {
         inFile.read(reinterpret_cast<char *>(inBuf), BLOCK_SIZE);
+        if(!inFile){
+            if(!inFile.eof()){
+                inFile.close();
+                outFile.close();
+                EVP_CIPHER_CTX_free(ctx);
+                return false;
+            }
+        }
+        
         size_t bytesRead = inFile.gcount();
 
-        // Encrypt the data
         if (EVP_EncryptUpdate(ctx, outBuf, &outLen, inBuf, bytesRead) != 1) {
             inFile.close();
             outFile.close();
@@ -104,6 +153,12 @@ bool encrypt_data ( const std::string & in_filename, const std::string & out_fil
             return false;
         }
         outFile.write(reinterpret_cast<const char *>(outBuf), outLen);
+        if (!outFile) {
+        inFile.close();
+        outFile.close();
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+        }
     }
 
     // Finalize the encryption
@@ -114,6 +169,12 @@ bool encrypt_data ( const std::string & in_filename, const std::string & out_fil
         return false;
     }
     outFile.write(reinterpret_cast<const char *>(outBuf), outLen);
+    if (!outFile) {
+        inFile.close();
+        outFile.close();
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
 
     // Clean up
     inFile.close();
@@ -124,19 +185,32 @@ bool encrypt_data ( const std::string & in_filename, const std::string & out_fil
 
 bool decrypt_data ( const std::string & in_filename, const std::string & out_filename, crypto_config & config )
 {
-    // Ensure that required parameters are set
     if (!config.m_crypto_function || !config.m_key || !config.m_key_len) {
         return false;
     }
+
+    OpenSSL_add_all_algorithms();
 
     EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
     if (!ctx) {
         return false;
     }
 
-    // Initialize the decryption operation
     const EVP_CIPHER *cipher = EVP_get_cipherbyname(config.m_crypto_function);
     if (!cipher) {
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    size_t iv_lenght = EVP_CIPHER_iv_length(cipher);
+    size_t key_lenght = EVP_CIPHER_key_length(cipher);
+
+    if((key_lenght > config.m_key_len)||(config.m_key == nullptr)){
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
+    if(((iv_lenght > 0) && (iv_lenght > config.m_IV_len))||((iv_lenght > 0)&&(config.m_IV == nullptr))){
         EVP_CIPHER_CTX_free(ctx);
         return false;
     }
@@ -146,14 +220,12 @@ bool decrypt_data ( const std::string & in_filename, const std::string & out_fil
         return false;
     }
 
-    // Open input file
     std::ifstream inFile(in_filename, std::ios::binary);
     if (!inFile.is_open()) {
         EVP_CIPHER_CTX_free(ctx);
         return false;
     }
 
-    // Open output file
     std::ofstream outFile(out_filename, std::ios::binary);
     if (!outFile.is_open()) {
         inFile.close();
@@ -161,19 +233,40 @@ bool decrypt_data ( const std::string & in_filename, const std::string & out_fil
         return false;
     }
 
-    // Copy the first 18 bytes (headers) without decryption
     char headerBuf[18];
     inFile.read(headerBuf, sizeof(headerBuf));
+    if(!inFile){
+        inFile.close();
+        outFile.close();
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
+
     outFile.write(headerBuf, sizeof(headerBuf));
+    if(!outFile){
+        inFile.close();
+        outFile.close();
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
 
     // Decryption buffer
-    const size_t BLOCK_SIZE = 16; // AES block size
+    const size_t BLOCK_SIZE = 16;
     uint8_t inBuf[BLOCK_SIZE];
     uint8_t outBuf[BLOCK_SIZE + BLOCK_SIZE];
 
     int outLen;
     while (!inFile.eof()) {
         inFile.read(reinterpret_cast<char *>(inBuf), BLOCK_SIZE);
+        if(!inFile){
+            if(!inFile.eof()){
+                inFile.close();
+                outFile.close();
+                EVP_CIPHER_CTX_free(ctx);
+                return false;
+            }
+        }
+        
         size_t bytesRead = inFile.gcount();
 
         // Decrypt the data
@@ -184,9 +277,14 @@ bool decrypt_data ( const std::string & in_filename, const std::string & out_fil
             return false;
         }
         outFile.write(reinterpret_cast<const char *>(outBuf), outLen);
+        if(!outFile){
+        inFile.close();
+        outFile.close();
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+        }
     }
 
-    // Finalize the decryption
     if (EVP_DecryptFinal_ex(ctx, outBuf, &outLen) != 1) {
         inFile.close();
         outFile.close();
@@ -194,6 +292,12 @@ bool decrypt_data ( const std::string & in_filename, const std::string & out_fil
         return false;
     }
     outFile.write(reinterpret_cast<const char *>(outBuf), outLen);
+    if(!outFile){
+        inFile.close();
+        outFile.close();
+        EVP_CIPHER_CTX_free(ctx);
+        return false;
+    }
 
     // Clean up
     inFile.close();
@@ -249,7 +353,6 @@ bool compare_files ( const char * name1, const char * name2 )
         if (bytesRead1 != bytesRead2 || memcmp(buffer1, buffer2, bytesRead1) != 0) {
             file1.close();
             file2.close();
-            cout << "fuck" << endl;
             return false;
         }
     } while (bytesRead1 > 0);
@@ -262,6 +365,7 @@ bool compare_files ( const char * name1, const char * name2 )
 
 int main ( void )
 {
+
 	crypto_config config {nullptr, nullptr, nullptr, 0, 0};
 
 	// ECB mode
@@ -270,6 +374,18 @@ int main ( void )
  	memset(config.m_key.get(), 0, 16);
 	config.m_key_len = 16;
 
+    crypto_config my_config {nullptr, nullptr, nullptr, 0, 0};
+
+	// ECB mode
+	my_config.m_crypto_function = "AES-128-ECB";
+	my_config.m_key = std::make_unique<uint8_t[]>(16);
+ 	memset(my_config.m_key.get(), 0, 16);
+    my_config.m_key_len = 16;
+
+    encrypt_data("homer-simpson.TGA", "tmp-TGA", my_config);
+    decrypt_data  ("tmp-TGA", "out_file.TGA", my_config);
+    assert(compare_files("out_file.TGA", "homer-simpson.TGA"));
+    
 	assert( encrypt_data  ("homer-simpson.TGA", "out_file.TGA", config) &&
 			compare_files ("out_file.TGA", "homer-simpson_enc_ecb.TGA") );
 
@@ -300,6 +416,15 @@ int main ( void )
 	config.m_IV_len = 16;
 	memset(config.m_IV.get(), 0, 16);
 
+    my_config.m_crypto_function = "AES-128-CBC";
+	my_config.m_IV = std::make_unique<uint8_t[]>(16);
+	my_config.m_IV_len = 20;
+	memset(my_config.m_IV.get(), 0, 16);
+    encrypt_data  ("UCM8.TGA", "tmp.TGA", my_config);
+    decrypt_data  ("tmp.TGA", "out_file.TGA", my_config);
+    assert(compare_files("out_file.TGA", "UCM8.TGA"));
+
+
 	assert( encrypt_data  ("UCM8.TGA", "out_file.TGA", config) &&
 			compare_files ("out_file.TGA", "UCM8_enc_cbc.TGA") );
 
@@ -324,6 +449,7 @@ int main ( void )
 	assert( decrypt_data ("image_8_enc_cbc.TGA", "out_file.TGA", config)  &&
 		    compare_files("out_file.TGA", "ref_8_dec_cbc.TGA") );
 	return 0;
+    
     
 }
 
